5e896da2d4b19faec78476c0f924ead10d2e5ba7
satishparaddi <paraddi.s@northeastern.edu>
2024-05-30 20:37:15 -0400

    initialize the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
-import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 export default function App() {
   return <BrowserRouter>
+  <Header />
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/sign-in" element={<SignIn />} />
     <Route path="/sign-up" element={<SignUp />} />
-    <Route path="/forgot-password" element={<ForgotPassword />} />
     <Route path="/about" element={<About />} />
     <Route path="/profile" element={<Profile />} />
   </Routes>
