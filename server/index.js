5433741795ab0bbc6cadf74e1f47d2548c5d964d
satishparaddi <paraddi.s@northeastern.edu>
2024-05-30 21:11:17 -0400

    initial setup of database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('Connected to MongoDB');
+})
+.catch((err)=>{
+    console.lof(err);
+});
 const app = express();
+
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
